693a37c278001381e8396d13dbbff32f3e394bb3
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-09-17 07:55:19 +0900

    [feat] : Add EquipmentBase & Weapon DA

---
+++ Source/FallenEra/ItemDatas/Equipments/FallenEraEquipmentItem.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "FallenEraItemBase.h"
+#include "FallenEraItemType.h"
+#include "FallenEraEquipmentItem.generated.h"
+
+
+UCLASS(Abstract, BlueprintType)
+class FALLENERA_API UFallenEraEquipmentItem : public UFallenEraItemBase
+{
+	GENERATED_BODY()
+	
+public:
+	// 장비 장착 슬롯
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Equipment|Base")
+	EEquipSlot EquipSlot;
+	
+	// ====================================================================
+	// 내구도 및 관리 수치 
+	// ====================================================================
+	
+	// 최대 내구도 (Stat_Durability_Max)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Equipment|Durability")
+	float MaxDurability = 100.0f;
+	
+	// 내구도 감소 배율 (Stat_Durability_DecreaseRate) - 닳는 속도 조절
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Equipment|Durability")
+	float DurabilityDecreaseRate = 1.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Equipment|Durability")
+	float RepairCostMultiplier = 1.0f;
+	
+	// ====================================================================
+	// 스탯 모디파이어 (방어력, 최대 체력 증가 등 캐릭터에게 주는 수치)
+	// ====================================================================
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Equipment|Stats",meta =(Categories="Stat"))
+	TMap<FGameplayTag, float> StatModifier;
+	
+};
---
+++ Source/FallenEra/ItemDatas/Equipments/FallenEraWeaponItem.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "FallenEraEquipmentItem.h"
+#include "FallenEraWeaponItem.generated.h"
+
+UCLASS(BlueprintType)
+class FALLENERA_API UFallenEraWeaponItem : public UFallenEraEquipmentItem
+{
+	GENERATED_BODY()
+public:
+	// ====================================================================
+	// 특수 전투 효과 확률 (상태이상 부여)
+	// ====================================================================
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Weapon|StatusEffects", meta=(Categories="Stat.Special"))
+	TMap<FGameplayTag,float> StatEffectChances;
+	
+	// ====================================================================
+	// 무기 전용 시청각 이펙트
+	// ====================================================================
+	
+	// 무기 휘두르는 소리 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item|Visual|SFX")
+	TSoftObjectPtr<class USoundBase> SwingSound;
+	
+	// 적을 타격했을 때 나는 소리 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item|Visual|SFX")
+	TSoftObjectPtr<class USoundBase> HitSound;
+};
