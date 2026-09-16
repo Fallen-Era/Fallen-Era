152df8f0313a4ce55b0fc43989204ead47518b48
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-09-17 08:16:20 +0900

    [feat] : Add Resource DA

---
+++ Source/FallenEra/ItemDatas/Resources/FallenEraResourceItem.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "FallenEraItemBase.h"
+#include "FallenEraResourceItem.generated.h"
+
+UCLASS(BlueprintType)
+class UFallenEraResourceItem : public UFallenEraItemBase
+{
+	GENERATED_BODY()
+public:
+	// TODO : 추후 확장성을 고려, 현재 부모 데이터만으로 충분
+};
