e9959bffb439250e8b3f1be7a3a3f3864d739d3a
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-09-17 07:55:34 +0900

    [feat] : Add Consume DA

---
+++ Source/FallenEra/ItemDatas/Consumes/FallenEraConsumableItem.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "FallenEraItemBase.h"
+#include "FallenEraConsumableItem.generated.h"
+
+UCLASS(BlueprintType)
+class UFallenEraConsumableItem : public UFallenEraItemBase
+{
+	GENERATED_BODY()
+	
+public:
+	// ====================================================================
+	// 섭취 효과
+	// ====================================================================
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Consumable|Effeacts", meta = (Categories="Stat"))
+	TMap<FGameplayTag,float> ConsumeEffects;
+	
+	// ====================================================================
+	// 소모품 전용 사운드
+	// ====================================================================
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item|Visual|SFX")
+	TSoftObjectPtr<USoundBase> ConsumeSound;
+};
